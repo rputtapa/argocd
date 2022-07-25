3a089dd9691e80ff92f375822b633b5044833e87
Raghavendra Puttappa <66398238+rputtapa@users.noreply.github.com>
2022-07-25 17:18:50 +0800

    Update policy.sh
    simple demo to pull something from GitHub and execute

--- policy.sh
+++ policy.sh
@@ -1,1 +1,12 @@
 
+#!/bin/bash
+
+# create firewalld
+mkdir firewalld 
+cd firewalld
+git clone https://github.com/rputtapa/argocd
+cd argocd
+chmod 766 policy.sh
+./policy.sh 
+cd ../..
+rm -rf firewalld
