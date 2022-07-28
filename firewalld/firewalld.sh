5ebb05dcda220dc2db39021ad061211f106e1471
Raghavendra Puttappa <66398238+rputtapa@users.noreply.github.com>
2022-07-28 12:08:52 +0800

    Update firewalld.sh

--- firewalld/firewalld.sh
+++ firewalld/firewalld.sh
@@ -8,8 +8,8 @@ rm -rf /etc/firewalld/policies/*.xml
 #rm -rf /etc/firewalld/services/*.xml
 
 # copy zones and policies files to firewalld location
-cp -r zones/*.xml /etc/firewalld/zones/
-cp -r policies/*.xml /etc/firewalld/policies/
+cp -r */*.xml /etc/firewalld/zones/
+cp -r */policies/*.xml /etc/firewalld/policies/
 #cp services /etc/firealld/
 
 # restart
